7f0d20088022d45049736bf1c6ef6331a3cc00fa
Spencer King <spencerking@users.noreply.github.com>
2018-05-06 23:38:29 -0500

    Ignore particles

--- jparse.rb
+++ jparse.rb
@@ -1,8 +1,13 @@
 require 'tiny_segmenter'
 
+# Ignore particles
+ignore = ["の", "に", "て", "は", "た", "が", "を", "で", "も", "と", "だ", "な", "か", "や"]
+
+# Read arguments
 input = ARGV
 content = File.read input[0]
 
+# Parse the input
 ts = TinySegmenter.new
 words = ts.segment(content, ignore_punctuation: true)
 
@@ -13,10 +18,12 @@ frequency = Hash.new(0)
 words.each { |word| frequency[word] += 1 }
 
 # Sort by the frequencies
-frequency = frequency.sort_by {|key, value| value}.to_h
+frequency = frequency.sort_by {|key, value| value}.reverse.to_h
 
 # Output to file
 frequency.each do |key,value|
-  str = key + ", " + value.to_s
-  File.open("freq.txt", 'a'){|file| file.puts str}
+	if !ignore.include? key
+  		str = key + ", " + value.to_s
+  		File.open("freq.txt", 'a'){|file| file.puts str}
+	end
 end
